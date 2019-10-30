396f2b513c9d7caf91df034faa3f9f4c4e235762
Dmitriy <chekanov.d@gmail.com>
2019-10-30 16:23:56 +0300

    End of task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import useJsonFetch from './hooks/useJsonFetch'
 
 function App() {
+
+  /*const url = "http://localhost:7070/data"*/
+  /*const url = "http://localhost:7070/error"*/
+  const url = "http://localhost:7070/loading"
+  const opts = {
+    method: 'GET'
+  }
+
+  const [data, loading, error] = useJsonFetch(url, opts);
+
+  console.log(data)
+  console.log(loading)
+  console.log(error)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      TEST
     </div>
   );
 }
---
+++ src/hooks/useJsonFetch.js
@@ -0,0 +1,33 @@
+import {useState, useEffect, useRef} from 'react';
+
+export default function useJsonFetch(url, opts) {
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+    const [hasError, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(url, opts)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            if (result.status === 'ok') {
+
+                setData(result)
+
+            } else {
+
+                setError(result)
+
+            }
+            setLoading(false)
+          },
+          (error) => {
+            setError(error)
+            setLoading(false)
+          }
+        )
+    }, [url]);
+
+    return [data, isLoading, hasError];
+}
